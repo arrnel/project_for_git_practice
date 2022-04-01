d12740d88df09f3ab02a3edd26267f514d709f9d
arrnel <101590632+arrnel@users.noreply.github.com>
2022-04-01 21:42:36 +0300

    Update Main.java

--- src/guru/qa/arrnel/Main.java
+++ src/guru/qa/arrnel/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, QA.GURU!!!");
     }
+    
+    void tempMethod(){
+        System.out.println("Temp Method");    
+    }
 }
